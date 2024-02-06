cf876bb3bcdf856eb20c755c078b25e8d1c3601e
gyu in <tlarb2022@naver.com>
2024-02-06 12:33:02 +0900

    commm

--- board-back/src/main/java/com/blogstudy/boardback/BoardBackApplication.java
+++ board-back/src/main/java/com/blogstudy/boardback/BoardBackApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BoardBackApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(BoardBackApplication.class, args);
 	}
 
