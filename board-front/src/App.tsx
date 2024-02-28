96b9364b6ed5da57811066aced0cf6d4f2ea27a5
gyu in <tlarb2022@naver.com>
2024-02-28 22:02:27 +0900

    commit

--- board-front/src/App.tsx
+++ board-front/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import BoardItem from 'components/BoardItem';
 import { commentListMock, favoriteListMock, latestBoardListMock, top3BoardListMock } from 'mocks';
@@ -8,9 +8,12 @@ import FavoriteItem from 'components/FavoriteItem';
 import InputBox from 'components/InputBox';
 
 function App() {
+
+ const [value, setValue] = useState<string>('');
+
   return (
     <>
-    <InputBox/>
+      <InputBox label='이메일' type='text' placeholder='이메일 주소를 입력해주세요' value={value} error={true} setValue={setValue} message='아무거나'/>
     {/* <div style={{ display: 'flex', columnGap: '30px', rowGap: '20px'}}>
         {favoriteListMock.map(favoriteListItem => <FavoriteItem favoriteListItem={favoriteListItem}/>)}
       </div> */}
--- board-front/src/components/InputBox/index.tsx
+++ board-front/src/components/InputBox/index.tsx
@@ -1,22 +1,53 @@
-import { forwardRef } from 'react';
+import { ChangeEvent, Dispatch, KeyboardEvent, SetStateAction, forwardRef } from 'react';
 import './style.css';
 
+//         interface: Input Box 컴포넌트 Properties                 //
 interface Props {
+   label: string;
+   type: 'text'|'password';
+   placeholder: string,
+   value: string;
+   setValue: Dispatch<SetStateAction<string>>;
+   error: boolean;  
 
+   icon?: string;
+   onButtonClick?: () => void;
+
+   message?: string;
+
+   onKeyDown?: (event: KeyboardEvent<HTMLInputElement>) => void;
 }
 
+//         component: Input Box 컴포넌트          //
 const InputBox = forwardRef<HTMLInputElement, Props>((props: Props, ref) =>  {
 
+   //         state: properties           //
+  const { label, type, placeholder, value, error, icon, message } = props;
+  const { setValue,onButtonClick, onKeyDown } = props;
+
+  //         event halnder : input 값 변경 이벤트  처리 함수//
+const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+  const { value } = event.target;
+  setValue(value);
+}
+
+const onKeyDownHandler = (event: KeyboardEvent<HTMLInputElement>) => {
+  if(!onKeyDown) return;
+  onKeyDown(event);
+}
+   //         render: Input Box 컴포넌트         //
     return (
      <div className='inputbox'>
-       <div className='inputbox-label'>{'비밀번호'}</div>
-       <div className='inputbox-container'>
-         <input className='input'/>
+       <div className='inputbox-label'>{label}</div>
+       <div className={error ? 'inputbox-container-error' : 'inputbox-container'}>
+         <input ref={ref} type={type} className='input' placeholder={placeholder} value={value} onChange={onChangeHandler} onKeyDown={onKeyDown} />
+           {onButtonClick !== undefined && (
            <div className='icon-button'>
-             <div className='icon eye-light-off-icon'></div> 
-           </div>     
+            {icon !== undefined && (<div className={`icon ${icon}`}></div>)}
+           </div>   
+           )}
        </div>
-       <div className='inputbox-message'>{'비밀번호는 8자 이상 입력해주세요.'}</div>
+       {message !== undefined && (<div className='inputbox-message'>{message}</div>)}      
     </div>
     )
 });
