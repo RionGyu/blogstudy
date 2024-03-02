22a4cbefd2e0baa8f4e72c5e866a7f7027e66c22
gyu in <tlarb2022@naver.com>
2024-03-02 13:59:26 +0900

    c

---
+++ board-front/src/layouts/Footer/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './style.css';
+
+//             component: 푸터 레이아웃          //
+export default function index() {
+
+
+//             render: 푸터 레이아웃  렌더링        //
+  return (
+    <div id='footer'>
+        <div className='footer-container'>
+            <div className='footer-top'>
+                <div className='footer-logo-box'>
+                    <div className='icon-box'>
+                        
+                    </div>
+                </div>
+                <div className='footer-link-box'></div>  
+            </div>
+            <div className='footer-bottom'>
+                <div></div>
+            </div>
+        </div>
+    </div>
+  )
+}
