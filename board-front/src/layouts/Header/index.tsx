1eaf1b42031ac2e708e44bf034b07444e3b4e404
gyu in <tlarb2022@naver.com>
2024-03-08 18:17:34 +0900

    commit

--- board-front/src/layouts/Header/index.tsx
+++ board-front/src/layouts/Header/index.tsx
@@ -1,20 +1,40 @@
 import React, { ChangeEvent, useRef, useState, KeyboardEvent, useEffect } from 'react';
 import './style.css';
-import { useNavigate, useParams } from 'react-router-dom';
-import { AUTH_PATH, MAIN_PATH, SEARCH_PATH, USER_PATH } from 'constant';
+import { useLocation,useNavigate, useParams } from 'react-router-dom';
+import { AUTH_PATH, BOARD_DETAIL_PATH, BOARD_WRITE_PATH, BOARD_UPDATE_PATH,  MAIN_PATH, SEARCH_PATH, USER_PATH } from 'constant';
 import { useCookies } from 'react-cookie';
 import useLoginUserStore from 'stores/login-user.store';
+import { useBoardStore } from 'stores';
 
 //        component: 헤더 레이아웃      //
 export default function Header() {
 
   //        state: 로그인 유저 상태             //
   const { loginUser, setLoginUser, resetLoginUser} = useLoginUserStore();
+  //        state: path 상태               //
+  const { pathname } = useLocation();
   //        state: cookie 상태             //
   const [cookies, setCookie] = useCookies();
 
   //        state: 로그인 상태             //
   const [isLogin, setLogin] = useState<boolean>(false);
+
+  //        state: 인증페이지 상태         //
+  const [isAuthPage, setAuthPage] = useState<boolean>(false);
+  //        state: 메인페이지 상태         //
+  const [isMainPage, setMainPage] = useState<boolean>(false);
+  //        state: 검색페이지 상태         //
+  const [isSearchPage, setSearchPage] = useState<boolean>(false);
+  //        state: 게시물 상세 페이지 상태         //
+  const [isBoardDetailPage, setBoardDetailPage] = useState<boolean>(false);
+  //        state: 게시물 작성 페이지 상태         //
+  const [isBoardWritePage, setBoardWritePage] = useState<boolean>(false);
+  //        state: 게시물 수정 페이지 상태         //
+  const [isBoardUpdatePage, setBoardUpdatePage] = useState<boolean>(false);
+  //        state: 유저 페이지 상태         //
+  const [isUserPage, setUserPage] = useState<boolean>(false);
+ 
+ 
   //        function: 네비게이트 함수     //
   const navigate = useNavigate();
 
@@ -101,7 +121,7 @@ const MyPageButton = () => {
   };
 
   //     event handler: 마이페이지 버튼 클릭 이벤트 처리 함수 //
-  const onSignButtonClickHandler = () => {
+  const onSignInButtonClickHandler = () => {
     navigate(AUTH_PATH());
   };
 
@@ -115,11 +135,43 @@ const MyPageButton = () => {
 
   
   //        render: 로그인 버튼 컴포넌트  렌더링 //
-  return <div className='black-button' onClick={onSignButtonClickHandler}>{'로그인'}</div>
+  return <div className='black-button' onClick={onSignInButtonClickHandler}>{'로그인'}</div>
+  
+};
+//        component: 업로드 버튼    컴포넌트  //
+const UploadButton = () => {
+
+  //         state: 게시물 상태         //
+  const { title, content, boardImageFileList, resetBoard } = useBoardStore();
+ 
+  //         event handler: 업로드 버튼 클릭 이벤트 처리 함수       //
+  const onUploadButtonClickHandler = () => {
+
+  }
+  //      render: 업로드 버튼 컴포넌트 렌더링          //
+  if (title && content)
+  return <div className='black-button' onClick={onUploadButtonClickHandler}>{'업로드'}</div>
+  //      render: 업로드 불가 버튼 컴포넌트 렌더링     //
+  return <div className='disable-button' >{'업로드'}</div>
   
 }
-
-
+//        effect: path가 변경될때 마다 실행될 함수        //
+useEffect(() => {
+  const isAuthPage = pathname.startsWith(AUTH_PATH());
+  setAuthPage(isAuthPage);
+  const isMainPage = pathname.startsWith(MAIN_PATH());
+  setMainPage(isMainPage);
+  const isSearchPage = pathname.startsWith(SEARCH_PATH(''));
+  setSearchPage(isSearchPage);
+  const isBoardDetailPage = pathname.startsWith(BOARD_DETAIL_PATH(''));
+  setBoardDetailPage(isBoardDetailPage);
+  const isBoardWritePage = pathname.startsWith(BOARD_WRITE_PATH());
+  setBoardWritePage(isBoardWritePage);
+  const isBoardUpdatePage = pathname.startsWith(BOARD_UPDATE_PATH(''));
+  setBoardUpdatePage(isBoardUpdatePage);
+  const isUserPage = pathname.startsWith(USER_PATH(''));
+  setUserPage(isUserPage);
+},[pathname]);
 //        render: 헤더 레이아웃  렌더링 //
   return (
     <div id='header'>
@@ -131,8 +183,9 @@ const MyPageButton = () => {
           <div className='header-logo'>{'gyus board'}</div>
         </div>
         <div className='header-right-box'>
-          <SearchButton />
-          <MyPageButton />
+          {(isAuthPage || isMainPage || isSearchPage || isBoardDetailPage) && <SearchButton />}
+          {(isMainPage || isSearchPage || isBoardDetailPage || isUserPage) && <MyPageButton />}
+          {(isBoardWritePage || isBoardUpdatePage) && <UploadButton />}
         </div>
       </div>      
     </div>
