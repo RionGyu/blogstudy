2cb60fcd16ec6122f5a5d3335f7970dd99eb7a6f
gyu in <tlarb2022@naver.com>
2024-03-14 21:52:01 +0900

    commit

--- board-front/src/views/Authentication/index.tsx
+++ board-front/src/views/Authentication/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './style.css';
+import InputBox from 'components/InputBox';
 
 //        component: 인증 화면 컴포넌트         //
 export default function Authentication() {
@@ -11,7 +12,28 @@ export default function Authentication() {
 
     //        render: SignInCard 컴포넌트 렌더링        //
     return(
-      <div className='auth-card'></div>
+      <div className='auth-card'>
+        <div className='auth-card-box'>
+          <div className='auth-card-top'>
+            <div className='auth-card-title-box'>
+              <div className='auth-card-title'>{'로그인'}</div>
+            </div>
+          </div>
+          <InputBox />
+          <InputBox />
+        </div>
+        <div className='auth-card-bottom'>
+          <div className='auth-sign-in-error-box'>
+            <div className='auth-sign-in-error-message'>
+              {'이메일 주소 또는 비밀번호를 잘못 입력했습니다.\n입력하신 내용을 다시 확인해주세요.'}
+            </div>
+          </div>
+          <div className='black-large-full-button'>{'로그인'}</div>
+          <div className='auth-description-box'>
+            <div className='auth-description'>{'신규 사용자이신가요?'}<span className='auth-description-link'>{'회원가입'}</span></div>
+          </div>
+        </div>
+      </div>
     )
   };
   //        component: 인증 화면 컴포넌트         //
