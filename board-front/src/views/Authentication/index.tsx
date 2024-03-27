80c33b3760a47f759eab88bafd2c1e6bf9f20ff2
gyu in <tlarb2022@naver.com>
2024-03-27 19:26:00 +0900

    sign-in make

--- board-front/src/views/Authentication/index.tsx
+++ board-front/src/views/Authentication/index.tsx
@@ -147,9 +147,102 @@ export default function Authentication() {
   //        component: 인증 화면 컴포넌트         //
   const SignUpCard = () => {
 
+    //        state: 이메일 요소 참조 상태       //
+    const emailRef = useRef<HTMLInputElement | null>(null);
+    //        state: 패스워드 요소 참조 상태       //
+    const passwordRef = useRef<HTMLInputElement | null>(null);
+    //        state: 패스워드 확인 요소 요소 참조 상태       //
+    const passwordCheckRef = useRef<HTMLInputElement | null>(null);
+    //        state: 페이지 번호 상태            //
+    const [page, setPage] = useState<1 | 2>(1);
+    //        state: 이메일 상태              //
+    const [email, setEmail] = useState<string>('');
+    //        state: 패스워드 상태            //
+    const [password, setPassword] = useState<string>('');
+    //        state: 패스워드 확인 상태       //
+    const [passwordCheck, setPasswordCheck] = useState<string>('');
+    //        state: 패스워드 타입 상태        //
+    const [passwordType, setPasswordType] = useState<'text' | 'password'>('password');
+    //        state: 패스워드 확인 타입 상태        //
+    const [passwordCheckType, setPasswordCheckType] = useState<'text' | 'password'>('password');
+    //        state: 이메일 에러 상태             //
+    const [isEmailError, setEmailError] = useState<boolean>(false);
+    //        state: 패스워드 에러 상태             //
+    const [isPasswordError, setPasswordError] = useState<boolean>(false);
+    //        state: 패스워드 확인 에러 상태             //
+    const [isPasswordCheckError, setPasswordCheckError] = useState<boolean>(false);
+    //        state: 이메일 에러 메시지 상태             //
+    const [emailErrorMessage, setEmailErrorMessage] = useState<string>('');
+    //        state: 패스워드 에러 메시지 상태             //
+    const [passwordErrorMessage, setPasswordErrorMessage] = useState<string>('');
+    //        state: 패스워드 확인 에러 메시지 상태             //
+    const [PasswordCheckErrorMessage, setPasswordCheckErrorMessage] = useState<string>('');
+
+    //        state: 패스워드 버튼 아이콘 상태                 //
+    const [passwordButtonIcon, setPasswordButtonIcon] = useState<'eye-light-off-icon' | 'eye-light-on-icon'>('eye-light-off-icon');
+
+    //        state: 패스워드 버튼 아이콘 상태                 //
+    const [passwordCheckButtonIcon, setPasswordCheckButtonIcon] = useState<'eye-light-off-icon' | 'eye-light-on-icon'>('eye-light-off-icon');
+
+    //        event handler: 이메일 변경 이벤트 처리        //
+    const onEmailChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+      const { value } = event.target;
+      setEmail(value);
+    }
+    //        event handler: 패스워드 변경 이벤트 처리        //
+    const onPasswordChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+      const { value } = event.target;
+      setPassword(value);
+    }
+    //        event handler: 패스워드 확인 변경 이벤트 처리        //
+    const onPasswordCheckChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+      const { value } = event.target;
+      setPasswordCheck(value);
+    }
+    //        event handler: 패스워드 확인 버튼 클릭 이벤트 처리        //
+    const onPasswordButtonClickHandler = () => {
+      if (passwordButtonIcon === 'eye-light-off-icon'){
+        setPasswordButtonIcon('eye-light-on-icon');
+        setPasswordType('text');
+      }
+      else {
+        setPasswordButtonIcon('eye-light-off-icon');
+        setPasswordType('password');
+      }
+    }
+    //        event handler: 패스워드 확인 변경 이벤트 처리        //
+    const onPasswordCheckButtonClickHandler = () => {
+      if (passwordCheckButtonIcon === 'eye-light-off-icon'){
+        setPasswordCheckButtonIcon('eye-light-on-icon');
+        setPasswordCheckType('text');
+      }
+      else {
+        setPasswordCheckButtonIcon('eye-light-off-icon');
+        setPasswordCheckType('password');
+      }
+    }
+    
     //        render: SignUpCard 컴포넌트    렌더링     //
     return(
-      <div className='auth-card'></div>
+      <div className='auth-card'>
+        <div className='auth-card-box'>
+          <div className='auth-card-top'>
+            <div className='auth-card-title-box'>
+              <div className='auth-card-title'>{'회원가입'}</div>
+              <div className='auth-card-page'>{'${page}/2'}</div>
+            </div>
+            <InputBox ref={emailRef} label='이메일 주소*' type='text' placeholder='이메일 주소를 입력해주세요.' value={email} onChange={onEmailChangeHandler} error={isEmailError} message={emailErrorMessage} />
+            <InputBox ref={passwordRef} label='비밀번호*' type={passwordType} placeholder='비밀번호를 입력해주세요.' value={password} onChange={onPasswordChangeHandler} error={isPasswordError} message={passwordErrorMessage} icon={passwordButtonIcon} onButtonClick={onPasswordButtonClickHandler}/>
+            <InputBox ref={passwordCheckRef} label='비밀번호 확인' type={passwordCheckType} placeholder='비밀번호를 다시 입력해주세요.' value={passwordCheck} onChange={onPasswordCheckChangeHandler} error={isPasswordCheckError} message={PasswordCheckErrorMessage} icon={passwordCheckButtonIcon} onButtonClick={onPasswordCheckButtonClickHandler}/>
+          </div>
+          <div className='auth-card-bottom'>
+            <div className='black-large-full-button'>{'다음 단계'}</div>
+            <div className='auth-description-box'>
+              <div className='auth-description' >{'이미 계정이 있으신가요? '}<span className='auth-description-link'>{'로그인'}</span></div>
+            </div>
+          </div>
+        </div>
+      </div>
     )
   };
   //        render: 인증 화면 컴포넌트         //
