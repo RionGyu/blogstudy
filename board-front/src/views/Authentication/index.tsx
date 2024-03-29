ed03fc764a81e5527ea98c7ba22bfcf9cc317dae
gyu in <tlarb2022@naver.com>
2024-03-29 19:53:05 +0900

    post API

--- board-front/src/views/Authentication/index.tsx
+++ board-front/src/views/Authentication/index.tsx
@@ -8,6 +8,7 @@ import { ResponseDto } from 'apis/response';
 import { useCookies } from 'react-cookie';
 import { useNavigate } from 'react-router-dom';
 import { MAIN_PATH } from 'constant';
+import { Address, useDaumPostcodePopup } from 'react-daum-postcode';
 
 //        component: 인증 화면 컴포넌트         //
 export default function Authentication() {
@@ -84,7 +85,7 @@ export default function Authentication() {
       signInRequest(requestBody).then(signInResponse);
     };
     //      event handler: 회원가입 버튼 클릭 이벤트 처리함수    //
-    const onSignUpLickClickHandler = () => {
+    const onSignUpLinkClickHandler = () => {
       setView('sign-up');
     };
 
@@ -137,7 +138,7 @@ export default function Authentication() {
             
             <div className='black-large-full-button' onClick={onSignInButtonClickHandler}>{'로그인'}</div>
             <div className='auth-description-box'>
-              <div className='auth-description'>{'신규 사용자이신가요?'}<span className='auth-description-link' onClick={onSignUpLickClickHandler}>{'회원가입'}</span></div>
+              <div className='auth-description'>{'신규 사용자이신가요?'}<span className='auth-description-link' onClick={onSignUpLinkClickHandler}>{'회원가입'}</span></div>
             </div>
           </div>
         </div>
@@ -178,6 +179,9 @@ export default function Authentication() {
     const [address, setAddress] = useState<string>('');
     //        state: 상세 주소 상태              //
     const [addressDetail, setAddressDetail] = useState<string>('');
+
+    //        state: 개인 정보 동의 상태         //
+    const [agreedPersonal, setAgreedPersonal] = useState<boolean>(false);
     //        state: 패스워드 타입 상태        //
     const [passwordType, setPasswordType] = useState<'text' | 'password'>('password');
     //        state: 패스워드 확인 타입 상태        //
@@ -195,6 +199,8 @@ export default function Authentication() {
     const [isTelNumberError, setTelNumberError] = useState<boolean>(false);
     //        state: 주소 에러 상태             //
     const [isAddressError, setAddressError] = useState<boolean>(false);
+     //        state: 개인 정보 동의 에러 상태             //
+     const [isAgreedPersonalError, setAgreedPersonalError] = useState<boolean>(false);
 
     //        state: 이메일 에러 메시지 상태             //
     const [emailErrorMessage, setEmailErrorMessage] = useState<string>('');
@@ -215,30 +221,44 @@ export default function Authentication() {
     //        state: 패스워드 버튼 아이콘 상태                 //
     const [passwordCheckButtonIcon, setPasswordCheckButtonIcon] = useState<'eye-light-off-icon' | 'eye-light-on-icon'>('eye-light-off-icon');
 
+
+    //        function: 다음 주소 검색 팝업 오픈 함수        //
+    const open = useDaumPostcodePopup();
+
     //        event handler: 이메일 변경 이벤트 처리        //
     const onEmailChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
       const { value } = event.target;
       setEmail(value);
+      setEmailError(false);
+      setEmailErrorMessage('');
     }
     //        event handler: 패스워드 변경 이벤트 처리        //
     const onPasswordChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
       const { value } = event.target;
       setPassword(value);
+      setPasswordError(false);
+      setPasswordErrorMessage('');
     }
     //        event handler: 패스워드 확인 변경 이벤트 처리        //
     const onPasswordCheckChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
       const { value } = event.target;
       setPasswordCheck(value);
+      setPasswordCheckError(false);
+      setPasswordCheckErrorMessage('');
     }
     //        event handler: 닉네임 변경 이벤트 처리        //
     const onNicknameChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
       const { value } = event.target;
       setNickname(value);
+      setNicknameError(false);
+      setNicknameErrorMessage('');
     }
     //        event handler: 핸드폰 번호 변경 이벤트 처리        //
     const onTelNumberChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
       const { value } = event.target;
       setTelNumber(value);
+      setTelNumberError(false);
+      setTelNumberErrorMessage('');
     }
     //        event handler: 주소 변경 이벤트 처리        //
     const onAddressChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
@@ -249,6 +269,12 @@ export default function Authentication() {
     const onAddressDetailChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
       const { value } = event.target;
       setAddressDetail(value);
+    }
+
+    //        event handler: 개인 정보 동의 체크 박스 버튼 클릭 이벤트 처리        //
+    const onAgreedPersonalClickHandler =() => {
+      setAgreedPersonal(!agreedPersonal);
+      setAgreedPersonalError(false);
     }
     //        event handler: 패스워드 버튼 클릭 이벤트 처리        //
     const onPasswordButtonClickHandler = () => {
@@ -274,7 +300,7 @@ export default function Authentication() {
     }
     //        event handler: 주소 버튼 클릭 이벤트 처리              //
     const onAddressButtonClickHandler = () => {
-
+      open({ onComplete });
     }
     //        event handler: 다음 단계 버튼 클릭 이벤트 처리          //
     const onNextButtonClickHandler = () => {
@@ -299,7 +325,7 @@ export default function Authentication() {
     }
     //        event handler: 회원 가입 버튼 클릭 이벤트 처리         //
     const onSignUpButtonClickHandler = () => {
-
+      alert('회원가입버튼!');
     }
 
     //        event handler: 로그인 링크 클릭 이벤트 처리 //
@@ -322,28 +348,42 @@ export default function Authentication() {
     //        event handler: 패스워드 확인 키 다운 이벤트 처리
     const onPasswordCheckKeyDownHandler = (event: KeyboardEvent<HTMLInputElement>) => {
       if (event.key !== 'Enter') return;
+      if (!nicknameRef.current) return;
       onNextButtonClickHandler();
+      nicknameRef.current.focus();
     }
     //        event handler: 닉네임 키 다운 이벤트 처리  //
     const onNicknameKeyDownHandler = (event: KeyboardEvent<HTMLInputElement>) => {
       if (event.key !== 'Enter') return;
+      if (!telNumberRef.current) return;
+      telNumberRef.current.focus();
       
     }
     //        event handler: 핸드폰 번호 키 다운 이벤트 처리  //
     const onTelNumberKeyDownHandler = (event: KeyboardEvent<HTMLInputElement>) => {
       if (event.key !== 'Enter') return;
-      
+      onAddressButtonClickHandler();
     }
     //        event handler: 주소 키 다운 이벤트 처리  //
     const onAddressKeyDownHandler = (event: KeyboardEvent<HTMLInputElement>) => {
       if (event.key !== 'Enter') return;
-      
+      if (!addressDetailRef.current) return;
+      addressDetailRef.current.focus();
     }
     //        event handler: 상세주소 키 다운 이벤트 처리  //
     const onAddressDetailKeyDownHandler = (event: KeyboardEvent<HTMLInputElement>) => {
       if (event.key !== 'Enter') return;
-      
+      onSignUpButtonClickHandler();
     }
+
+    //        event handler: 다음 주소 검색 완료 이벤트 처리
+    const onComplete = (data: Address) => {
+      const { address } = data;
+      setAddress(address);
+      if (!addressDetailRef.current) return;
+      addressDetailRef.current.focus();
+    }
+ 
     
     //        render: SignUpCard 컴포넌트    렌더링     //
     return(
@@ -377,8 +417,10 @@ export default function Authentication() {
             {page === 2 && (
             <>
             <div className='auth-consent-box'>
-              <div className='auth-check-box'></div>
-              <div className='auth-consent-title'>{'개인정보동의'}</div>
+              <div className='auth-check-box' onClick={onAgreedPersonalClickHandler}>
+                <div className={`icon ${agreedPersonal ? 'check-round-fill-icon' : 'check-ring-light-icon'}`}></div>           
+              </div>
+              <div className={isAgreedPersonalError ? 'auth-consent-title-error' : 'auth-consent-title'} >{'개인정보동의'}</div>
               <div className='auth-consent-link'>{'더보기 >'}</div>
             </div>
             <div className='black-large-full-button' onClick={onSignUpButtonClickHandler}>{'회원 가입'}</div>
@@ -411,3 +453,5 @@ export default function Authentication() {
     </div>
   )
 }
+
+
